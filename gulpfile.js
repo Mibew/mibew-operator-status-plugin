84c94f427a55b2cf466de59b0f82d99921069479
Fedor A. Fetisov <faf@ossg.ru>
2017-03-30 16:00:47 +0300

    Fix invalid bitmask for release archives

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('prepare-release', function() {
             .pipe(tar('operator-status-plugin-' + version + '.tar'))
             .pipe(gzip())
     )
-    .pipe(chmod(0644))
+    .pipe(chmod(644))
     .pipe(gulp.dest('release'));
 });
 
